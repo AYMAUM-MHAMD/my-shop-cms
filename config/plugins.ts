7222e659ac8ca784f30b265d7a292193efadbcfb
ausi <mntskeaese@gmail.com>
2025-10-02 17:45:36 +0200

    cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,17 @@
-export default {
+export default ({ env }) => ({
+  // ✅ إعدادات رفع الملفات عبر Cloudinary
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+    },
+  },
+
+  // ✅ إعدادات users-permissions كما كانت سابقاً
   'users-permissions': {
     config: {
       jwt: {
@@ -6,4 +19,4 @@ export default {
       },
     },
   },
-};
+});
